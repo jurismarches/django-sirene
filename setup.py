5470b0eb8c4d5aa66a048f64ffd2f955fcf87b0b
Roxane <roxane.bellot@gmail.com>
2020-06-03 09:41:27 +0200

    update package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sirene',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         "django-bulk-update>=2.2.0",
