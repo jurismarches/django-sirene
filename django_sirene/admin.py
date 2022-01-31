3a3869b80682060a584b4207c51692edd77e36e5
Maxime Morinière <m.moriniere@octopusmind.info>
2022-01-31 09:07:58 +0100

    [admin] Fix performance issues by using raw_id_fields for the headquarter field

--- django_sirene/admin.py
+++ django_sirene/admin.py
@@ -3,28 +3,25 @@ from .models import Institution
 
 
 class InstitutionAdmin(admin.ModelAdmin):
-    search_fields = (
-        'siret',
-        "name",
-        "commercial_name"
-    )
+    search_fields = ("siret", "name", "commercial_name")
     list_select_related = (
-        'legal_status',
-        'headquarter',
-        'municipality',
-        'activity',
+        "legal_status",
+        "headquarter",
+        "municipality",
+        "activity",
     )
     list_display = (
-        'siret',
-        '__str__',
-        'zipcode',
-        'municipality',
-        'headquarter',
-        'is_headquarter',
-        'is_expired',
-        'updated',
-        'created',
+        "siret",
+        "__str__",
+        "zipcode",
+        "municipality",
+        "headquarter",
+        "is_headquarter",
+        "is_expired",
+        "updated",
+        "created",
     )
+    raw_id_fields = ("headquarter",)
 
 
 admin.site.register(Institution, InstitutionAdmin)
