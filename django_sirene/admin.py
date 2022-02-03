5e80fb0df9171e8a650559ec51070609b270abfd
mmoriniere <m.moriniere@octopusmind.info>
2022-02-03 10:52:24 +0100

    [admin] Fix performance issues by using raw_id_fields for the headquarter field + add admin for other models (#4)
    * [admin] Fix performance issues by using raw_id_fields for the headquarter field
    
    * Update version
    
    * [admin] More fields as raw id
    
    * exclude headquarter field to make sure this is why it breaks
    
    * Add admin for Activity, LegalStatus and Municipality + make the related fields as autocomplete for Institution

--- django_sirene/admin.py
+++ django_sirene/admin.py
@@ -1,30 +1,53 @@
 from django.contrib import admin
-from .models import Institution
+from .models import Activity, Institution, LegalStatus, Municipality
 
 
 class InstitutionAdmin(admin.ModelAdmin):
-    search_fields = (
-        'siret',
-        "name",
-        "commercial_name"
-    )
+    search_fields = ("siret", "name", "commercial_name")
     list_select_related = (
-        'legal_status',
-        'headquarter',
-        'municipality',
-        'activity',
+        "legal_status",
+        "headquarter",
+        "municipality",
+        "activity",
     )
     list_display = (
-        'siret',
-        '__str__',
-        'zipcode',
-        'municipality',
-        'headquarter',
-        'is_headquarter',
-        'is_expired',
-        'updated',
-        'created',
+        "siret",
+        "__str__",
+        "zipcode",
+        "municipality",
+        "headquarter",
+        "is_headquarter",
+        "is_expired",
+        "updated",
+        "created",
     )
+    autocomplete_fields = ("activity", "legal_status", "municipality")
+    raw_id_fields = ("headquarter",)
 
 
 admin.site.register(Institution, InstitutionAdmin)
+
+
+class DjangoSireneBaseAdmin:
+    search_fields = ("code", "name")
+
+
+class ActivityAdmin(DjangoSireneBaseAdmin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Activity, ActivityAdmin)
+
+
+class LegalStatusAdmin(DjangoSireneBaseAdmin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LegalStatus, LegalStatusAdmin)
+
+
+class MunicipalityAdmin(DjangoSireneBaseAdmin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Municipality, MunicipalityAdmin)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sirene',
-    version='0.2',
+    version='3.0.3',
     packages=find_packages(),
     install_requires=[
         "django-bulk-update>=2.2.0",
